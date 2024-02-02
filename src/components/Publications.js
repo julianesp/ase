5eeb6d6b8b0c8fa2199fd7c837322dba25c35d6f
Julián España <julii1295@gmail.com>
2024-02-01 21:59:04 -0500

    Localizando ruta de estilos to publicaciones

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import styles from '@/styles/Publications.scss'
+import styles from "@/styles/Publications.module.scss"
 
 const Publicaciones = () => {
     return (
