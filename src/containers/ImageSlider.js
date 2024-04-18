ff267d175c1c336751d63df8e0724c98a18c7db3
Julián España <julii1295@gmail.com>
2024-04-17 21:44:56 -0500

    Transición de imagenes establecida

--- src/containers/ImageSlider.js
+++ src/containers/ImageSlider.js
@@ -6,47 +6,35 @@ import styles from '../styles/ImageSlider.module.css';
 
 const ImageSlider = ({ imagePaths, enableTransition = true }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const [canChangeImage, setCanChangeImage] = useState(true);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (canChangeImage) {
-                goToNextSlide();
-            }
-        }, 3000); // Cambia de imagen cada 3 segundos
-
-        return () => clearInterval(interval); // Limpia el temporizador al desmontar el componente
-    }, [currentImageIndex, canChangeImage]);
 
     const goToPreviousSlide = () => {
-        if (canChangeImage) {
-            setCurrentImageIndex(prevIndex =>
-                prevIndex === 0 ? imagePaths.length - 1 : prevIndex - 1
-            );
-            setCanChangeImage(false);
-            setTimeout(() => setCanChangeImage(true), 3000); // Habilita el cambio después de 3 segundos
-        }
+        setCurrentImageIndex(prevIndex =>
+            prevIndex === 0 ? imagePaths.length - 1 : prevIndex - 1
+        );
     };
 
     const goToNextSlide = () => {
-        if (canChangeImage) {
-            setCurrentImageIndex(prevIndex =>
-                prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1
-            );
-            setCanChangeImage(false);
-            setTimeout(() => setCanChangeImage(true), 3000); // Habilita el cambio después de 3 segundos
-        }
+        setCurrentImageIndex(prevIndex =>
+            prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1
+        );
     };
 
-    const transitionStyle = enableTransition ? styles.transition : '';
+    useEffect(() => {
+        let interval;
+        if (enableTransition) {
+            interval = setInterval(goToNextSlide, 5000);
+        }
+
+        return () => clearInterval(interval);
+    }, [currentImageIndex, enableTransition]);
 
     return (
-        <div className={`${styles.image} ${transitionStyle}`}>
+        <div className={styles.image}>
             <Image
                 src={imagePaths[currentImageIndex]}
                 alt="Slide"
-                width={300} // Ajusta el ancho según tus necesidades
-                height={200} // Ajusta la altura según tus necesidades
+                width={300}
+                height={200}
             />
 
             <div className={styles.buttonContainer}>
@@ -61,7 +49,7 @@ const ImageSlider = ({ imagePaths, enableTransition = true }) => {
 
                 <button onClick={goToNextSlide} className={styles.right}>
                     <Image
-                        alt='Arrow left'
+                        alt='Arrow right'
                         src={right}
                         width={20}
                         height={20}
