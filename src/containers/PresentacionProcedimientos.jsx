7061b5d5ca05194c59fb5426de201095aa9859dc
Julián España <julii1295@gmail.com>
2024-07-14 17:44:30 -0500

    Empezando a modificar componente con enlaces para Facebook

--- src/containers/PresentacionProcedimientos.jsx
+++ src/containers/PresentacionProcedimientos.jsx
@@ -9,30 +9,30 @@ import styles from "../styles/PresentacionProcedimientos.scss";
 import imageData from "../../data/imageSlider"; // Importa el archivo de datos
 
 const Procedimientos = ({ enableTransition = true }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const goToPreviousSlide = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? imageData.length - 1 : prevIndex - 1
-    );
-  };
+const goToPreviousSlide = () => {
+  setCurrentImageIndex((prevIndex) =>
+    prevIndex === 0 ? imageData.length - 1 : prevIndex - 1
+  );
+};
 
-  const goToNextSlide = useCallback(() => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === imageData.length - 1 ? 0 : prevIndex + 1
-    );
-  }, []);
+const goToNextSlide = useCallback(() => {
+  setCurrentImageIndex((prevIndex) =>
+    prevIndex === imageData.length - 1 ? 0 : prevIndex + 1
+  );
+}, []);
 
-  useEffect(() => {
-    let interval;
-    if (enableTransition) {
-      interval = setInterval(goToNextSlide, 5000);
-    }
+useEffect(() => {
+  let interval;
+  if (enableTransition) {
+    interval = setInterval(goToNextSlide, 5000);
+  }
 
-    return () => clearInterval(interval);
-  }, [enableTransition, goToNextSlide]);
+  return () => clearInterval(interval);
+}, [enableTransition, goToNextSlide]);
 
-  const currentImage = imageData[currentImageIndex];
+const currentImage = imageData[currentImageIndex];
 
   return (
     <div className={styles.image}>
@@ -48,13 +48,94 @@ const Procedimientos = ({ enableTransition = true }) => {
         </button>
       </div>
 
-      <div className={styles.overlayButton}>
-        <a href={currentImage.url} target="_blank" rel="noopener noreferrer">
-          Abrir enlace
-        </a>
-      </div>
+<div className={styles.overlayButton}>
+  <a href={currentImage.url} target="_blank" rel="noopener noreferrer">
+    Abrir enlace
+  </a>
+</div>
     </div>
   );
 };
 
 export default Procedimientos;
+
+// "use client";
+
+// import { useState, useEffect, useCallback } from "react";
+// import Image from "next/image";
+// import left from "../../public/left.png";
+// import right from "../../public/right.png";
+// import imageData from "../../data/imageSlider";
+// import styles from "../styles/PresentacionProcedimientos.scss";
+
+// const PresentacionOperaciones = ({
+//   imagePaths = [],
+//   enableTransition = true,
+// }) => {
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   const goToPreviousSlide = () => {
+//     setCurrentImageIndex((prevIndex) =>
+//       prevIndex === 0 ? imageData.length - 1 : prevIndex - 1
+//     );
+//   };
+
+//   const goToNextSlide = useCallback(() => {
+//     setCurrentImageIndex((prevIndex) =>
+//       prevIndex === imageData.length - 1 ? 0 : prevIndex + 1
+//     );
+//   }, []);
+
+//   useEffect(() => {
+//     let interval;
+//     if (enableTransition) {
+//       interval = setInterval(goToNextSlide, 5000);
+//     }
+
+//     return () => clearInterval(interval);
+//   }, [enableTransition, goToNextSlide]);
+
+//   const currentImage = imageData[currentImageIndex];
+
+//   return (
+//     <section className={styles.image}>
+//       <Image
+//         src={imagePaths[currentImageIndex]}
+//         alt="Slide"
+//         width={300}
+//         height={200}
+//         priority={true}
+//       />
+
+//       <div className={styles.buttonContainer}>
+//         <button onClick={goToPreviousSlide} className={styles.left}>
+//           <Image
+//             alt="Arrow left"
+//             src={left}
+//             width={20}
+//             height={20}
+//             priority={true}
+//           />
+//         </button>
+
+//         <button onClick={goToNextSlide} className={styles.right}>
+//           <Image
+//             alt="Arrow right"
+//             src={right}
+//             width={20}
+//             height={20}
+//             priority={true}
+//           />
+//         </button>
+//       </div>
+
+//       <div className={styles.overlayButton}>
+//         <a href={currentImage.url} target="_blank" rel="noopener noreferrer">
+//           Abrir enlace
+//         </a>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default PresentacionOperaciones;
