1411aa5596f2b2327479672d9e1ca6448bf41836
Julián España <julii1295@gmail.com>
2024-08-13 22:25:13 -0500

    Eliminando líneas de next.config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const path = require("path");
 
 const nextConfig = {
-  output: "standalone",
-  images: {
-    // unoptimized: true,
-    // domains: ["firebasestorage.googleapis.com"],
-    // remotePatterns: ["firebasestorage.googleapis.com"],
-  },
-
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.module.rules.push({
@@ -23,10 +16,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-
-  // experimental: {
-  //   isrMemoryCacheSize: 0,
-  // },
 };
 
 module.exports = nextConfig;
