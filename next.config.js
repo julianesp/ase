78510e12abfbdb6772cbf097ae0e8be88908a51a
Julián España <julii1295@gmail.com>
2024-08-27 12:08:27 -0500

    Cambiando configuración de archivo nextjs.yml para poder desplegar sitio

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-// // next.config.js
-// const path = require("path");
-
-// const nextConfig = {
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       config.module.rules.push({
-//         test: /\.module\.s(a|c)ss$/,
-//         loader: "sass-loader",
-//         options: {},
-//       });
-//     }
-//     return config;
-//   },
-
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-// };
-
-// module.exports = nextConfig;
-
 const path = require("path");
 
 const nextConfig = {
@@ -37,15 +15,6 @@ const nextConfig = {
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-  },
-  // Asegúrate de que las páginas se generen dinámicamente
-  experimental: {
-    appDir: true,
-  },
-  // Si necesitas alguna configuración específica para imágenes
-  images: {
-    // Ajusta esto según tus necesidades
-    domains: ['https://console.firebase.google.com/u/0/project/aliriose-3a721/storage/aliriose-3a721.appspot.com/files/~2Fimages?hl=es'], 
   },
 };
 
