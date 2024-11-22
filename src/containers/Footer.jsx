e91327aba5e903de3859dc1c506ea93c2ff67d5a
Julián España <julii1295@gmail.com>
2024-11-21 20:35:02 -0500

    Corrigiendo ubicación de elementos

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import face from "../../public/assets/redes/facebook.png";
@@ -14,57 +14,120 @@ import styles from "../styles/Footer.module.scss";
 
 const Contacto = () => {
   const [menuOption, setMenuOptions] = useState(false);
-  const [isClient, setIsClient] = useState(false);
-  const [animate, setAnimate] = useState(false);
-
-  // Asegurarse de que el código se ejecute en el cliente
-  useEffect(() => {
-    setIsClient(true);
-
-    // Configurar intervalo para activar la animación cada 10 segundos
-    const interval = setInterval(() => {
-      setAnimate(true);
-      setTimeout(() => setAnimate(false), 1000); // Desactivar la animación después de 1 segundo
-    }, 2000);
-
-    return () => clearInterval(interval); // Limpiar intervalo al desmontar
-  }, []);
+  const menuRef = useRef(null);
+  // const [isClient, setIsClient] = useState(false);
+  // const [animate, setAnimate] = useState(false);
+  const flechaRef = useRef(null);
 
   const switchOptions = () => {
     setMenuOptions(!menuOption);
   };
 
+  // Función para cerrar el menú cuando se hace clic fuera
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        flechaRef.current &&
+        !flechaRef.current.contains(event.target)
+      ) {
+        setMenuOptions(false); // Oculta las redes sociales si haces clic fuera
+      }
+    };
+
+    // Detectar clics en toda la ventana
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      // Limpiar eventos al desmontar el componente
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [menuRef]);
+
   return (
     <main className={styles.footer}>
-      <section
-        className={`${styles.flecha} ${animate ? styles.animate : ""}`}
+      <article
+        ref={flechaRef}
+        className={styles.flecha}
         onClick={switchOptions}
       >
         <Image alt="Links to navigation" src={arrow} priority />
-      </section>
-      <section className={styles.llamar}>
+      </article>
+
+      <article className={styles.llamar}>
         <Link href="tel:+573138627818">
           <Image alt="Contáctame" src={call} priority />
         </Link>
-      </section>
+      </article>
 
-      <section className={styles.logo}>
+      <article className={styles.logo}>
         <Link href="/">
           <Image src={logo} alt="Logo" />
         </Link>
-      </section>
+      </article>
 
-      <section className={styles.description}>
+      <article className={styles.description}>
         <p>Servicio de cirugía general</p>
-      </section>
+      </article>
 
-      <section className={styles.doctor}>
+      <article className={styles.doctor}>
         <p>Dr. Alirio Solarte España</p>
         <p>Médico cirujano</p>
         <p>Sibundoy - Putumayo</p>
-      </section>
+      </article>
 
-      {isClient && (
+      <article
+        ref={menuRef} // Referencia para el menú de redes sociales
+        className={`${styles.redes} ${menuOption ? styles.open : styles.closed}`}
+      >
+        <ul>
+          <li>
+            <Link
+              href="https://www.facebook.com/profile.php?id=100085485673809"
+              target="_blank"
+              rel="noreferrer"
+              passHref
+            >
+              <Image alt="Facebook" src={face} priority />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.instagram.com/alexriob/"
+              target="_blank"
+              rel="noreferrer"
+              passHref
+            >
+              <Image alt="Instagram" src={ins} priority />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://wa.me/573174503604"
+              target="_blank"
+              rel="noreferrer"
+              passHref
+            >
+              <Image alt="Whatsapp" src={wha} priority />
+            </Link>
+          </li>
+          <li className={styles.tiktok}>
+            <Link
+              href="https://www.tiktok.com/@julii1295?_t=8n2OQ52Q4aD&_r=1"
+              target="_blank"
+              rel="noreferrer"
+              passHref
+            >
+              <Image alt="TikTok" src={tik} priority />
+            </Link>
+          </li>
+        </ul>
+      </article>
+
+      {/* {isClient && (
         <div
           className={`${styles.redes} ${
             menuOption ? styles.open : styles.closed
@@ -116,7 +179,7 @@ const Contacto = () => {
             </li>
           </ul>
         </div>
-      )}
+      )} */}
 
       <div className={styles.externo}>
         <Link
